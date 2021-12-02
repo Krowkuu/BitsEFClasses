1ea42ca83444f7fb33252102a57ea538a708846f
Krowkuu <lancej@my.lanecc.edu>
2021-12-02 15:48:59 -0800

    Removed everything but bitscontext and appconfig since those are all i need, created an NUnit test thingy

---
+++ BitsTests/AppConfigTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace BitsTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+        }
+    }
+}
